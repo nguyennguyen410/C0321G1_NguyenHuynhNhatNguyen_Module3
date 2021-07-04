ebd1a20c7380b82da336e085c78c9bd401da266f
nguyennguyen410 <nguyen.nhn89@gmail.com>
2021-07-04 18:36:05 +0700

    module3 bai14_v01

---
+++ 14_dependency_management_&_packaging/bai_tap/bt_phuong_trinh_bac_hai_demo/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        PhuongTrinhBacHai.giaiPhuongTrinhBacHai(0,2,3);
+        PhuongTrinhBacHai.giaiPhuongTrinhBacHai(2,2,2);
+        PhuongTrinhBacHai.giaiPhuongTrinhBacHai(2,1,-1);
+    }
+}
